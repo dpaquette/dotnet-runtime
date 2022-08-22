a3c22653ffba8f956d7dbce4e3315ab700c40477
dkurepa <dkurepa@microsoft.com>
2022-08-22 13:47:16 +0200

    update depencencies from the emsdk build

--- eng/common/tools.ps1
+++ eng/common/tools.ps1
@@ -365,17 +365,10 @@ function InitializeVisualStudioMSBuild([bool]$install, [object]$vsRequirements =
 
   # If the version of msbuild is going to be xcopied,
   # use this version. Version matches a package here:
-  # https://dev.azure.com/dnceng/public/_packaging?_a=package&feed=dotnet-eng&package=RoslynTools.MSBuild&protocolType=NuGet&version=17.2.1&view=overview
-  $defaultXCopyMSBuildVersion = '17.2.1'
+  # https://dev.azure.com/dnceng/public/_packaging?_a=package&feed=dotnet-eng&package=RoslynTools.MSBuild&protocolType=NuGet&version=17.1.0&view=overview
+  $defaultXCopyMSBuildVersion = '17.1.0'
 
-  if (!$vsRequirements) {
-    if (Get-Member -InputObject $GlobalJson.tools -Name 'vs') {
-      $vsRequirements = $GlobalJson.tools.vs
-    }
-    else {
-      $vsRequirements = New-Object PSObject -Property @{ version = $vsMinVersionReqdStr }
-    }
-  }
+  if (!$vsRequirements) { $vsRequirements = $GlobalJson.tools.vs }
   $vsMinVersionStr = if ($vsRequirements.version) { $vsRequirements.version } else { $vsMinVersionReqdStr }
   $vsMinVersion = [Version]::new($vsMinVersionStr)
 
